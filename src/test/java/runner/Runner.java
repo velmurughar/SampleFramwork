ebe22a2b370726927e787195d3073d975eae8040
bala <abitex2022@gmail.com>
2022-11-26 11:55:01 +0530

    Fullproject Upload

---
+++ src/test/java/runner/Runner.java
@@ -0,0 +1,14 @@
+package runner;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(	features = {"src/test/java/features/login.feature"}, 
+					glue = {"stepDefinition"}, 
+					monochrome = true,
+					dryRun = false,
+					publish = true)
+
+public class Runner extends AbstractTestNGCucumberTests {
+
+}
